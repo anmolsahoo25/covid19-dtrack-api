7fe3c43ca67ce406234690ff46bb0770c9c168ba
Anmol Sahoo <anmol.sahoo25@gmail.com>
2020-03-24 18:35:57 +0530

    added sqlalchemy

--- app/app.py
+++ app/app.py
@@ -1,5 +1,16 @@
+import os
+
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+db = SQLAlchemy(app)
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    make = db.Column(db.String(80), nullable = False)
 
 @app.route('/')
 def hello_world():
