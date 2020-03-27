165f20721ab8b5c9df6c6f81c1a7d00f52f80604
Anmol Sahoo <anmol.sahoo25@gmail.com>
2020-03-27 12:56:36 +0530

    added suspected case table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -10,10 +10,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-class Car(db.Model):
+class SuspectedCase(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    make = db.Column(db.String(80), nullable = False)
+    device_mac_addr = db.Column(db.String(17), nullable = False)
+    diagnosed_or_suspected = db.Column(db.Boolean(), nullable = False)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/cases', methods=['GET', 'POST'])
+def update_cases():
+    if request.method == 'GET':
+        # return list of all cases
+        raw_cases = SuspectedCase.query.all()
+        cases = list(map(lambda x : {
+          'mac' : x.device_mac_addr,
+          'diag_or_susp' : x.diagnosed_or_suspected}, raw_cases))
+        return {'cases' : cases}
+    elif request.method == 'POST':
+        # create new case
+        mac = request.form['mac']
+        diag_or_susp = bool(request.form['diag_or_susp'])
+        case_to_add = SuspectedCase(device_mac_addr = mac, diagnosed_or_suspected = diag_or_susp)
+        db.session.add(case_to_add)
+        db.session.commit()
+        return {'msg' : 'created'}
